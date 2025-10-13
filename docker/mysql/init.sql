69426e089d03ec279100cefb2d3d5363fe578189
spva <vadims (at) gmx.de>
2025-10-13 18:45:44 +0200

    added test table into init.sql file

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,7 +1,7 @@
 -- init.sql for Symfony development environment
 
 -- Additional test database
---- CREATE DATABASE IF NOT EXISTS symfony_db;
+CREATE DATABASE IF NOT EXISTS symfony_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- User for tests
 CREATE USER 'symfony'@'%' IDENTIFIED BY 'password';
@@ -9,3 +9,8 @@ GRANT ALL PRIVILEGES ON symfony_db.* TO 'symfony'@'%';
 
 -- Initial reference data
 USE symfony_db;
+
+CREATE TABLE IF NOT EXISTS test (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
